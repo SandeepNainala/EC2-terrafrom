70f1f4edaa2e1a530cd24414e9bb2ea2563841b2
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-10-20 19:06:53 +0530

    Ec2 terraform - Jenkins,Trivy,Sonar

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.large"
+  key_name      = key-login
+  vpc_security_group_ids = [ aws_security_group.Jenkins-sg.id ]
+  user_data = templatefile("./install.sh", {})
+   
+
+  tags = {
+    Name = "Jenkins-sonar"
+  }
+
+
+root_block_device {
+    volume_size = 30
+  }
+}
+
+resource "aws_security_group" "Jenkins-sg" {
+    name = "Jenkins-sg"
+    description = "Allow TLS inbound rules"
+
+    ingress = [ 
+        for port in[ 22, 80, 8080, 443, 9000 ] : {
+          description      = "inbound rules"
+          from_port        = port
+          to_port          = port
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = []
+          prefix_list_ids  = []
+          security_group   = []
+          self             = false
+        }
+     ]
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
+  }
+}
+  
